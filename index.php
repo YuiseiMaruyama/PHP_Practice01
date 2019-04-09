245b2d3a6cc766919341c7d794906428d1d87a9c
YuiseiMaruyama <yuisei.rock@gmail.com>
2019-04-09 22:24:45 +0900

    add new file

--- index.php
+++ index.php
@@ -6,8 +6,14 @@
 // "" 特殊文字(\n \t) 変数
 // ''
 
-$name = 'taguchi';
-$s1 = "hello $name \n hello again!";
-$s2 = 'hello $name\nhello again!';
-var_dump($s1);
-var_dump($s2);
+// $name = 'taguchi';
+// $s1 = "hello $name!\n hello again!";
+// $s1 = "hello {$name} !\n hello again!";
+// $s1 = "hello ${name} !\n hello again!";
+// $s2 = 'hello $name\nhello again!';
+// var_dump($s1);
+// var_dump($s2);
+
+// 連結 .
+$s = "hello" . "world";
+var_dump($s);
