bbcef0636515dc72ccade72758d4c1ba2190d126
YuiseiMaruyama <yuisei.rock@gmail.com>
2019-04-10 03:25:43 +0900

    add new file

--- index.php
+++ index.php
@@ -1,28 +1,27 @@
-<!--  便利な組み込み関数を使ってみよう -->
-
-<!-- 関数 -->
+<!--  クラスを作ってみよう -->
 
 <?php
 
-// $x = 5.6;
-//ceil() : 小数点以下を切り上げる
-// echo ceil($x);//6
-// floor() : 小数点以下を切り捨てる
-// echo floor($x);//5
-// round() : 四捨五入
-// echo round($x);//6
-// rand() : 乱数生成
-// echo rand(1,10);//1-10
+// クラスが持つ変数：プロパティ
+// クラスが持つ関数：メソッド
 
-// $s1 = "hello";
-// $s2 = "ねこ";
-// echo strlen($s1);//5
-// echo mb_strlen($s2);//2
-//$s1はstring型なので%s、$xはfloor型なので%f
-// printf("%s - %s - %.3f",$s1,$s2,$x);
+class User{
+    //property
+    public $name;
 
-$colors = ["red","blue","yellow"];
-//配列の要素をカウント
-echo count($colors);//3
-//配列の要素を指定した区切り文字で連結
-echo implode("@",$colors);
+    //constructor
+    public function __construct($name){
+        $this->name = $name;
+    }
+    //method
+    public function sayHi(){
+        echo "hi, i am $this->name!";
+    }
+}
+
+$tom = new User("Tom");
+$bob = new User("Bob");
+
+echo $tom->name; //Tom
+$bob->sayHi(); // hi i am Bob
+
