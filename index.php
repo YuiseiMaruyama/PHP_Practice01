dc2d2299e71f488e10731969262aeabea0cbce39
YuiseiMaruyama <yuisei.rock@gmail.com>
2019-04-09 22:31:37 +0900

    add new file

--- index.php
+++ index.php
@@ -1,19 +1,17 @@
-<!-- 文字列を扱ってみよう -->
+<!-- if文で条件分岐をしてみよう -->
 
 <?php
 
-// 文字列
-// "" 特殊文字(\n \t) 変数
-// ''
+// if 条件分岐
+// 比較演算子　> < >= <= == === != !==
+// 論理演算子　and && , or || , !
 
-// $name = 'taguchi';
-// $s1 = "hello $name!\n hello again!";
-// $s1 = "hello {$name} !\n hello again!";
-// $s1 = "hello ${name} !\n hello again!";
-// $s2 = 'hello $name\nhello again!';
-// var_dump($s1);
-// var_dump($s2);
+$score = 40;
 
-// 連結 .
-$s = "hello" . "world";
-var_dump($s);
+if($score > 80){
+    echo "great!";
+}else if($score > 60){
+    echo "good!";
+}else{
+    echo "so so...";
+}
