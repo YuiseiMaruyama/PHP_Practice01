e09eacf5799b2c4c6bc16cad4abd741aefbcbbd7
YuiseiMaruyama <yuisei.rock@gmail.com>
2019-04-09 18:31:02 +0900

    add new file

--- index.php
+++ index.php
@@ -1,17 +1,14 @@
-<!-- 変数とデータ型について理解しよう -->
+<!-- 定数を使ってみよう -->
 
 <?php
 
-// 変数：データにつけるラベルで、phpでは　$変数名　で定義する
-// 文字列：string
-// 数値： integer float
-// 論理値： boolean / true false
-// 配列
-// オブジェクト
-// null
+// 定数：変更されない値につけるラベル
+// define("定数名","値")
+// 定数名は大文字
+define("MY_EMAIL","taguchi@dotinstall.com");
 
-//phpではいちいちデータ型を宣言する必要がない
-$msg = "hello from the TOP!";
-echo $msg;
-var_dump($msg);
+echo MY_EMAIL;
 
+var_dump(__LINE__); //現在の行数
+var_dump(__FILE__); //ファイル名
+var_dump(__DIR__); // ディレクトリ名
