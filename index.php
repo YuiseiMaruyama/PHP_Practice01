06754fdaa3eecd145f64f97211f1d05c3b902508
YuiseiMaruyama <yuisei.rock@gmail.com>
2019-04-09 22:22:33 +0900

    add new file

--- index.php
+++ index.php
@@ -1,25 +1,13 @@
-<!-- 数値の演算をしてみよう -->
+<!-- 文字列を扱ってみよう -->
 
 <?php
 
-// 
-// 数値の演算
-// + - * / % **
+// 文字列
+// "" 特殊文字(\n \t) 変数
+// ''
 
-// $x = 10 % 3;  // 1
-// $y = 30.2 /4; // 7.55
-// var_dump($x);
-// var_dump($y);
-
-//単項演算子
-// $z = 5;
-// $z++; //6
-// var_dump($z);
-// $z--; //5
-// var_dump($z);
-
-//代入を伴う演算子
-$x = 5;
-// $x = $x + 2;
-$x *= 2;//10
-var_dump($x);
+$name = 'taguchi';
+$s1 = "hello $name \n hello again!";
+$s2 = 'hello $name\nhello again!';
+var_dump($s1);
+var_dump($s2);
