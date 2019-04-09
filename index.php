2ef839d6656b3748ca28cee6ba69fa0eaed9263f
YuiseiMaruyama <yuisei.rock@gmail.com>
2019-04-09 18:09:28 +0900

    add new file

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
 <?php
 
-echo "hello! Again!";
+echo "hello from the TOP!";
+
+?>
+
+<!DOCTYPE html>
+<html lang= "ja">
+<head>
+    <meta charset="utf-8">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css">
+    <script src="main.js"></script>
+</head>
+<body>
+    <p>Hello World<?php echo  "from PHP"; ?></p>
+</body>
+</html>
