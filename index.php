be5b8290fa51a8354b7baf64b38fbfeafc377c0b
YuiseiMaruyama <yuisei.rock@gmail.com>
2019-04-10 15:45:24 +0900

    add new file***

--- index.php
+++ index.php
@@ -1,40 +1,41 @@
-<!--  アクセス権について理解しよう -->
+<!--  staticキーワードを使ってみよう -->
 
 <?php
 
-// アクセス権
-// - private : そのクラスからのみアクセス可能
-// - protected : そのクラス+親子クラスからのみアクセス可能
-// - public  : どこからでもアクセス可能
+// static : インスタンス化させずに定義する
 
 class User{
-    //property
-    // public $name;
-    // private $name;
-    protected $name;//Userでは使いたくないが、AdminUserで使いたい
-
+    public $name;
+    public static $count = 0;
 
     //constructor
     public function __construct($name){
         $this->name = $name;
+        //同じクラス内でクラス名を使う時には self が使える
+        //self = User
+        self::$count++;
     }
     //method
-    //finalは継承先に上書きされないようにする
     public function sayHi(){
         echo "hi, i am $this->name!";
     }
-}
-
-class AdminUser extends User{
-    public function sayHello(){
-        echo "hello from $this->name";
+    //インスタンスを作らないので引数　$nameなどは使えない
+    public static function getMessage(){
+        echo "hello from User class!";
     }
 }
+    
+//static にアクセスするには :: をつける
+// User::getMessage();
 
 $tom = new User("Tom");
+$bob = new User("Bob");
+
+echo User::$count;//2
+
 // echo $tom->name;
 
-$steve = new AdminUser("Steve");
-$steve -> sayHello();
+// $steve = new AdminUser("Steve");
+// $steve -> sayHello();
 
 
