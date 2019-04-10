06fae817e66a429e5bb1ee1cbd709b014681fc36
YuiseiMaruyama <yuisei.rock@gmail.com>
2019-04-11 02:50:50 +0900

    add new file****

--- index.php
+++ index.php
@@ -1,13 +1,19 @@
-<!--  Cookieを使ってみよう -->
+<!--  セッションを使ってみよう -->
 <?php
-//cookie : ブラウザにデータが保存される
 
-// setcookie("username","taguchi");
-// setcookie("username","taguchi",time()+60*60);//第3引数で有効期限を指定
-
-//cookieの削除
-// setcookie("username","taguchi",time()-60*60);
-
-echo $_COOKIE['username'];
+//cookieは気軽に使える一方、ブラウザ側にデータを保存するので改ざんされる可能性がある
+//セッションはサーバー側にデータを保存するので大容量のデータを保存できる、改ざんされない
 
 
+// session
+//サーバー側
+
+session_start();
+
+// $_SESSION['username'] = 'taguchi';
+
+// echo $_SESSION['username'];
+
+// unset($_SESSION['username']);
+
+echo $_SESSION['username'];
